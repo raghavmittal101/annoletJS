aeaf448ed768660ca93cd8cc9938c85a8de6a876
Raj Manvar <raj.manvar@students.iiit.ac.in>
2016-06-14 11:33:31 +0530

    debugging

--- annolet_main.js
+++ annolet_main.js
@@ -73,7 +73,6 @@ function get_phonetics(str){
 //main function which will execute other functions
 function annolet_main() {
     console.log('hello world annolet');
-    get_phonetics('between the world we are stuck');
     annolet_createContainer();
     document.onclick = function(event) {
         if (event === undefined) {
