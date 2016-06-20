a6ee5dad5bebe726112f1aecf40f0e47e91c015f
Raghav Mittal <raghavmittal101@gmail.com>
2016-06-20 16:29:35 +0530

    sticky note ability added

--- annolet_main.js
+++ annolet_main.js
@@ -1,8 +1,9 @@
 
 var $j = jQuery.noConflict();
 
-annolet_main();
+annolet_main();  //excecuting main function
 
+//-------------------------------------------------------------------
 var annolet_btn;
 // function to create annolet controls container
 function annolet_createContainer() {
@@ -24,10 +25,11 @@ function annolet_createContainer() {
     "<span id='annolet' style='border-radius:10px;  color:orange;font-weight:bold;font-family:monospace; font-size:1.3em'>AnnoLet!</span>"+
     "<span id='annolet' style='color:grey;'>|</span>"+
     "<li id='annolet' class=annolet-tools-menu-item id=highlight-btn onclick='annolet_btn=1;'>TagIt!</li>"+
+    "<li id='annolet' class=annolet-tools-menu-item id=note-btn onclick='annolet_btn=2;'>Add Note</li>"+
     "<li id='annolet' class=annolet-tools-menu-item id=exit-btn onclick='annolet_btn=0;'>exit</li>"+
     "</ul>"; //HTML to create a list of options
 }
-
+//-------------------------------------------------------------------
 // function to get Xpath to passed element
 function anno_getXpathTo(element) {
     if (element.id !== '') {
@@ -48,12 +50,13 @@ function anno_getXpathTo(element) {
         }
     }
 }
-
+//-------------------------------------------------------------------
 // function to evaluate element from Xpath
 function anno_getElementByXpath(xpath) {
     return document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
 
+//-------------------------------------------------------------------
 //main function which will execute other functions
 function annolet_main() {
     disableAllLinks()  // it will disable all the links present in webpage iteratively
@@ -68,9 +71,12 @@ function annolet_main() {
         if (annolet_btn === 1) {
             anno_highlight(xpath);
         }
+        else if(annolet_btn == 2){
+          anno_addNote();
+        }
     };
 }
-
+//-------------------------------------------------------------------
 // funtion to disable all links
 function disableAllLinks(){
     var anchors = document.getElementsByTagName("a");
@@ -78,7 +84,7 @@ function disableAllLinks(){
         anchors[i].onclick = function() {return(false);};
     }
 }
-
+//-------------------------------------------------------------------
 //function to store tags into JSON object.
 var annolet_obj = 0; //will save jason objects
 function annolet_insertIntoObject(xpath) {
@@ -95,7 +101,7 @@ function annolet_insertIntoObject(xpath) {
     tagObject(xpath, annolet_obj);
   }
 }
-
+//-------------------------------------------------------------------
 // function for creation of objects
 function tagObject(xpath, obj){
   tagName = prompt('tagName:');
@@ -108,7 +114,7 @@ function tagObject(xpath, obj){
     }
   )
 }
-
+//-------------------------------------------------------------------
 //function for highlighting element
 function anno_highlight(xpath) {
     clicked_element = anno_getElementByXpath(xpath)
@@ -121,4 +127,79 @@ function anno_highlight(xpath) {
       $j(anno_getElementByXpath(xpath)).wrapInner("<span id='mark' style='background:yellow;'></span>");
       annolet_insertIntoObject(xpath); // storing into object
     }
+}
+//-------------------------------------------------------------------
+//function to add sticky note kind of notediv
+function anno_addNote(){
+  spawn();
+
+      var markup =
+        '<div class="annolet_box annolet_note annolet_background-gray">' +
+        '<div class="annolet_box-header">' +
+        '<a href="#" class="annolet_left annolet_add annolet_hidden">&plus;</a>' +
+        '<a href="#" class="annolet_right annolet_remove annolet_hidden">&times;</a>' +
+        '</div><textarea class="annolet_note-text"></textarea></div>';
+
+    var spawn = function () {
+        $("body").append(makeNote($(markup)));
+    };
+
+    var remove = function () {
+        $(this).parent().parent().remove();
+    };
+
+    var resizeTextArea = function () {
+        var self = $(this);
+        var spaceToGrab = 60;
+        self.find('.annolet_note-text').height(self.height() - 45);
+    };
+
+    var hideButtons = function (element) {
+        $(element).find("a").hide();
+    };
+
+    var showButtons = function (element) {
+        $(element).find("a").show();
+    };
+
+    var save = function (id, value, position) {
+        console.log(id); // position
+        console.log(value);
+        console.log(position);
+    };
+
+    var makeNote = function (element) {
+        var $note = $j(element);
+        $note.resizable({
+            handles: "se"
+        });
+        $note.draggable({
+            handle: ".annolet_box-header"
+        });
+
+        $text = $note.find('textarea');
+        $text.focusout(function (e) {
+            save($(this).oid, $(this).val(), $(this).parent().position());
+        });
+        $note.click(function () {
+            $(this).find('textarea').focus();
+        });
+
+        $note.mouseover(function () {
+            showButtons(this);
+        });
+        $note.mouseout(function () {
+            hideButtons(this);
+        });
+
+        $note.find(".add").click(spawn);
+        $note.find(".remove").click(remove);
+        $note.resize(resizeTextArea);
+        hideButtons($note);
+        return $note;
+    };
+
+    $(".annoet_note").each(function (i, e) {
+        makeNote(e);
+    });
 }
