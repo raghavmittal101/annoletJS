24506517a57fc3fc92dfc4239a4df0b8ec571e6d
Raj Manvar <raj.manvar@students.iiit.ac.in>
2016-06-27 10:31:19 +0530

    ready to merge

--- annolet_main.js
+++ annolet_main.js
@@ -24,6 +24,9 @@ function annolet_createContainer() {
     "<span id='annolet' style='border-radius:10px;  color:orange;font-weight:bold;font-family:monospace; font-size:1.3em'>AnnoLet!</span>"+
     "<span id='annolet' style='color:grey;'>|</span>"+
     "<li id='annolet' class=annolet-tools-menu-item id=highlight-btn onclick='annolet_btn=1;'>TagIt!</li>"+
+    /*"<li id='annolet' class=annolet-tools-menu-item id=highlight-btn onclick='annolet_btn=2;'>Highlight</li>"+*/
+    "<li id='annolet' class=annolet-tools-menu-item id=highlight-btn onclick='annolet_btn=3;'>Phonetics</li>"+
+    "<li id='annolet' class=annolet-tools-menu-item id=highlight-btn onclick='annolet_btn=4;'>Translation</li>"+
     "<li id='annolet' class=annolet-tools-menu-item id=exit-btn onclick='annolet_btn=0;'>exit</li>"+
     "</ul>"; //HTML to create a list of options
 }
@@ -54,12 +57,12 @@ function anno_getElementByXpath(xpath) {
     return document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
 
-
+//------------------------------------------------------------------------
 var phonetic_trans = "default_value";
 var language_trans = "default_value";
 
 function get_phonetics(str){
-  
+
   var xhr = new XMLHttpRequest();
   xhr.open("POST", "//localhost:5000/phonetic-trans", true); // replace localhost afterwards.
   xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
@@ -80,9 +83,8 @@ function get_phonetics(str){
 
 }
 
-
 function get_languagetrans(str,fr,to){
-  
+
   var xhr = new XMLHttpRequest();
   xhr.open("POST", "//localhost:5000/language-translive", true); // replace localhost afterwards
   xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
@@ -103,9 +105,87 @@ function get_languagetrans(str,fr,to){
 }
 
 
+
+//function for getting phonetic
+function anno_phonetic(xpath) {
+  clicked_element = anno_getElementByXpath(xpath);
+  if (clicked_element.id == "mark" || clicked_element.id == "annolet") {
+      console.log('not permitted');
+  }
+  else {
+    //if element is already translated
+  if (anno_getElementByXpath(xpath).id != "phonetic" || !(anno_getElementByXpath(xpath).id)) {
+    var text_to_translate = $j(anno_getElementByXpath(xpath)).html();
+    get_phonetics(text_to_translate);
+    var timer = window.setInterval
+    (
+      function ()
+      {
+        if(typeof phonetic_trans !== "default_value")
+        {
+          console.log("text changing");
+          $j(anno_getElementByXpath(xpath)).text(phonetic_trans);
+          phonetic_trans = "default_value";
+          window.clearInterval(timer);
+        }
+        else
+        {
+          console.log("returned without change");
+        }
+      }
+      ,1000
+    );
+  }
+  else {
+        console.log('already translated');
+    }
+  }
+}
+
+
+//function for getting phonetic
+function anno_language(xpath) {
+  clicked_element = anno_getElementByXpath(xpath);
+  //if element is already highlighted
+  if (clicked_element.id == "mark" || clicked_element.id == "annolet") {
+      console.log('not permitted');
+  }
+  else {
+  //if element is already translated
+  if (anno_getElementByXpath(xpath).id != "language" || !(anno_getElementByXpath(xpath).id)) {mark
+    var text_to_translate = $j(anno_getElementByXpath(xpath)).html();
+    get_languagetrans(text_to_translate,'en','hi');
+    var timer = window.setInterval
+    (
+      function ()
+      {
+        if(typeof language_trans !== "default_value")
+        {
+          console.log("text changing");
+          $j(anno_getElementByXpath(xpath)).text(language_trans);
+          language_trans = "default_value";
+          window.clearInterval(timer);
+        }
+        else
+        {
+          console.log("returned without change");
+        }
+      }
+      ,1000
+    );
+  }
+  else {
+        console.log('already translated');
+    }
+  }
+}
+
+//------------------------------------------------------------------------
+
+
+
 //main function which will execute other functions
 function annolet_main() {
-  annolet_createContainer();
     disableAllLinks()  // it will disable all the links present in webpage iteratively
     annolet_createContainer();
     document.onclick = function(event) {
@@ -114,11 +194,16 @@ function annolet_main() {
         } // for IE
         var target = 'target' in event ? event.target : event.srcElement; // for IE
         var root = document.compatMode === 'CSS1Compat' ? document.documentElement : document.body;
-      var xpath = anno_getXpathTo(target);
-      if (annolet_btn === 1) {
-        anno_highlight(xpath);
-      }
-
+        var xpath = anno_getXpathTo(target);
+        if (annolet_btn === 1) {
+            anno_highlight(xpath);
+        }
+        if(annolet_btn === 4){
+          anno_language(xpath);
+        }
+        else if (annolet_btn == 3){
+          anno_phonetic(xpath);
+        }
     };
 }
 
@@ -160,12 +245,9 @@ function tagObject(xpath, obj){
   )
 }
 
-
-
 //function for highlighting element
 function anno_highlight(xpath) {
-
-  clicked_element = anno_getElementByXpath(xpath);
+    clicked_element = anno_getElementByXpath(xpath);
     //if element is already highlighted
     if (clicked_element.id == "mark" || clicked_element.id == "annolet") {
         console.log('not permitted');
@@ -176,78 +258,3 @@ function anno_highlight(xpath) {
       annolet_insertIntoObject(xpath); // storing into object
     }
 }
-
-
-
-
-//function for getting phonetic
-function anno_phonetic(xpath) {
-    //if element is already translated
-  if (anno_getElementByXpath(xpath).id != "phonetic" || !(anno_getElementByXpath(xpath).id)) {
-    var text_to_translate = $j(anno_getElementByXpath(xpath)).html();
-    get_phonetics(text_to_translate);
-    var timer = window.setInterval
-    (
-      function ()
-      {
-        if(typeof phonetic_trans !== "default_value")
-        {
-          console.log("text changing");
-          $j(anno_getElementByXpath(xpath)).text(phonetic_trans);
-          phonetic_trans = "default_value";
-          window.clearInterval(timer);
-        }
-        else
-        {
-          console.log("returned without change");
-        }
-      }
-      ,1000
-    );
-  }
-  else {
-        console.log('already translated');
-    }
-}
-
-
-//function for getting phonetic
-function anno_language(xpath) {
-  //if element is already translated
-  if (anno_getElementByXpath(xpath).id != "language" || !(anno_getElementByXpath(xpath).id)) {
-    var text_to_translate = $j(anno_getElementByXpath(xpath)).html();
-    get_languagetrans(text_to_translate,'en','hi');
-    var timer = window.setInterval
-    (
-      function ()
-      {
-        if(typeof language_trans !== "default_value")
-        {
-          console.log("text changing");
-          $j(anno_getElementByXpath(xpath)).text(language_trans);
-          language_trans = "default_value";
-          window.clearInterval(timer);
-        }
-        else
-        {
-          console.log("returned without change");
-        }
-      }
-      ,1000
-    );
-  }
-  else {
-        console.log('already translated');
-    clicked_element = anno_getElementByXpath(xpath)
-    //if element is already highlighted
-    if (clicked_element.id == "mark" || clicked_element.id == "annolet") {
-        console.log('not permitted');
-    }
-    else {
-      // hightlight selected element and store it
-      $j(anno_getElementByXpath(xpath)).wrapInner("<span id='mark' style='background:yellow;'></span>");
-      annolet_insertIntoObject(xpath); // storing into object
-    }
-}
-
-  
