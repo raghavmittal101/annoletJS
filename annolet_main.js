2d2e18a1e0d44c9ff88db31e868e7ecb5901cb4e
Raghav Mittal <raghavmittal101@gmail.com>
2016-07-04 13:18:42 +0530

    js modified

--- annolet_main.js
+++ annolet_main.js
@@ -1,15 +1,6 @@
 //main function which will execute other functions
 function annolet_main() {
     disableAllLinks()  // it will disable all the links present in webpage iteratively
-    document.onclick = function(event) {
-        if (event === undefined) {
-            event = window.event;
-        } // for IE
-        var target = 'target' in event ? event.target : event.srcElement; // for IE
-        var root = document.compatMode === 'CSS1Compat' ? document.documentElement : document.body;
-        var xpath = annolet.xpath.get(target);
-        console.log(xpath);
-    };
 }
 
 // funtion to disable all links
@@ -20,5 +11,5 @@ function disableAllLinks(){
     }
 }
 
-disableAllLinks();
+// calling main function
 annolet_main();
